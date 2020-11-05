58d09fcc36c2cea744b25b6934f2a0b840150ddb
rmusale1 <rmusale1@binghamton.edu>
2020-11-05 03:27:58 -0500

    adding dev and production url

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports={
+    MongoURI: 'mongodb+srv://socialMediaApp:Myapp@1234@cluster0.dkqfs.mongodb.net/cluster0?retryWrites=true&w=majority',
+    GoogleClientID:'273227219035-u0vlof6egv2u9pr54m1sjv39n06dg6kp.apps.googleusercontent.com',
+    GoogleClientSecret: 'PjEFQ-ILWaw-X-ZF5cbX31iz' 
+}
